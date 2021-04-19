8bedab417243cfb62c5698101a4c8a062f351e97
Omkar Jadhav <omkarj@wemotiveforge.com>
2021-04-19 12:39:15 +0530

    added console , cmmts

--- src/Components/post/post.js
+++ src/Components/post/post.js
@@ -1,28 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import AllPost from '../AllPost/allpost'
 import axios from 'axios'
+// fake api
 let usersEndpoint = "https://jsonplaceholder.typicode.com/users";
-let source = axios.CancelToken.source();
+// cancel token
+const CancelToken = axios.CancelToken;
+const source = CancelToken.source();
  const Post =(props)=>{
+     // states
      const [post,setPost]=useState([])
+     
+     // for api get res
      useEffect(()=>{
         axios.get(usersEndpoint,{cancelToken: source.token})
         .then(res=>{
             const posts = res.data.slice( 0, 4 );
-            const updatedPosts = posts.map( post => {
-                return {
-                    ...post               
-                 }
-            } );
+            const updatedPosts = posts.map( post => {return {...post}});
             setPost([updatedPosts])
         })
         .catch(err=>{
             console.log(err)
         })
         return () => {
-            if (source) source.cancel("Landing Component got unmounted")
+           source && source.cancel("Landing Component got unmounted")
+            console.log("Cancel")
           }
-     })
+     },[])
+     // for showing rendered post
      let showPosts=null;
     showPosts=post.map(data=>data.map(sdata=><AllPost key={sdata.id} id={sdata.id} title={sdata.name} />))
      return (
