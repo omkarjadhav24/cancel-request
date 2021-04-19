a13787fdcb8a7f2b0e22862c33eed355248f7a82
Omkar Jadhav <omkarj@wemotiveforge.com>
2021-04-19 12:39:47 +0530

    fix- removed unused import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
 import Nav from '../src/Components/nav/nav'
-import Post from '../src/Components/post/post'
 function App() {
   return (
     <>
